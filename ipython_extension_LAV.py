ac0b1e76c4a6e511f914889b9ade8bd062d14402
Gil Georges <gilgeorges@users.noreply.github.com>
2016-03-07 14:07:17 +0100

    initial import

---
+++ ipython_extension_LAV.py
@@ -0,0 +1,15 @@
+from ipython_extension import pgMagics
+import os
+
+
+def load_ipython_extension(ipython):
+    # The `ipython` argument is the currently active `InteractiveShell`
+    # instance, which can be used in any way. This allows you to register
+    # new magics or aliases, for example.
+    magics = pgMagics(ipython,
+                      default_host="postgres-server",
+                      default_port=5433,
+                      default_sslcert=os.path.join(os.path.expanduser("~"),
+                                                   "certfile.crt"),
+                      default_user=os.getlogin())
+    ipython.register_magics(magics)
